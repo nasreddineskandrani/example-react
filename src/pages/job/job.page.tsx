60e5a4ccf1a1562fc1e1c95b09f131b817ebe147
nas <nasreddine.skandrani@gmail.com>
2018-04-01 20:55:35 -0400

    fix selector and add counter span in dom

--- src/pages/job/job.page.tsx
+++ src/pages/job/job.page.tsx
@@ -9,9 +9,15 @@ import { incrementEpic$ } from './+state/job.epic';
 import { epics$ } from '../../store'; // TODO fix use absolute
 import { removeAsyncReducer, injectAsyncReducer, store } from '../../store'; // TODO fix use absolute
 
-const mapStateToProps = (state: JobState) => {
+const storeKeyJobPage = 'async_job';
+
+const mapStateToProps = (state: {}) => {
+  if (!state[storeKeyJobPage]) {
+    return {};
+  }
+  const jobPageState: JobState = state[storeKeyJobPage];
   return {
-    count: state.counter
+    counter: jobPageState.counter
   };
 };
 
@@ -24,7 +30,7 @@ const mapDispatchToProps = (dispatch: (action: PlainAction) => void) => {
 
 export interface JobPageProps {
   dispatch: (action: PlainAction) => void;
-  count: number;
+  counter: number;
   onIncrement: () => void;
   unloadPage: () => void;
 }
@@ -32,12 +38,12 @@ export interface JobPageProps {
 export class JobPage extends React.Component<JobPageProps, {}> {
 
   componentDidMount() {
-    injectAsyncReducer(store, 'async_job', JobReducer);
+    injectAsyncReducer(store, storeKeyJobPage, JobReducer);
     epics$.next(incrementEpic$);
   }
 
   componentWillUnmount() {
-    removeAsyncReducer(store, 'async_job');
+    removeAsyncReducer(store, storeKeyJobPage);
     this.props.unloadPage();
   }
 
@@ -48,6 +54,9 @@ export class JobPage extends React.Component<JobPageProps, {}> {
         <button type="button" onClick={this.props.onIncrement}>
           {`Increment`}
         </button>
+        <span>
+          {this.props.counter}
+        </span>
       </div>
     );
   }
